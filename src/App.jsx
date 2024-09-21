f7ab5d4f570b667dcd16529afdd8098bc03c1cbb
kunals12 <work.kunal12@gmail.com>
2024-09-21 19:33:50 +0530

    feat: requesting airdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React, { FC, useMemo, useState } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import {RequestAirdrop} from './Airdrop';
 
 function App() {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const connectWallet = async () => {
-
-  }
-  const requestAirdrop = async () => {
-
-  }
-
-
-
   return (
-    <ConnectionProvider endpoint={endpoint}> 
-            <WalletProvider wallets={[]} autoConnect>
-                <WalletModalProvider>
-                  <div>
-                  <WalletMultiButton />
-                  <WalletDisconnectButton />
-                  </div>
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            <RequestAirdrop />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
